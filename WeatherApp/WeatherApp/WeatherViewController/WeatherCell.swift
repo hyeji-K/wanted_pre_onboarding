89b292389c76efa9bc42cc4cd1068caf220feee3
Khyeji <khjji0502@gmail.com>
2022-06-14 19:46:46 +0900

    Add: weatherViewController에 collectionView를 사용하여 뷰 구현

--- WeatherApp/WeatherApp/WeatherViewController/WeatherCell.swift
+++ WeatherApp/WeatherApp/WeatherViewController/WeatherCell.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class WeatherCell: UICollectionViewCell {
     
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var currentHumidLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.layer.cornerRadius = 10
+        contentView.backgroundColor = .gray
+    }
+    
+//    func configure(_ weatherInfo: WeatherInfo) {
+//
+//    }
 }
--- WeatherApp/WeatherApp/WeatherViewController/WeatherViewController.swift
+++ WeatherApp/WeatherApp/WeatherViewController/WeatherViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension WeatherViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as? WeatherCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
-    */
+}
 
+extension WeatherViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let margin: CGFloat = 10
+        let width = collectionView.bounds.width
+        let availableWidth = width - (margin * 2)
+        
+        return CGSize(width: availableWidth, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
 }
